22efbb836c54cf6bbcf4423d027682f2e2a6696c
minhnguyenjirnexu <minh.nguyen@jirnexu.com>
2020-01-03 10:53:24 +0700

    Initial

---
+++ precache-manifest.aa4ea09b0513a09ec255ac08a40e2b8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "204c4bebf7bff4bd56d7370977d8c849",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2159f910084f0a362f9b",
+    "url": "/static/css/main.08bfa54b.chunk.css"
+  },
+  {
+    "revision": "d5ff3581b8f660187d41",
+    "url": "/static/js/2.51486a74.chunk.js"
+  },
+  {
+    "revision": "2159f910084f0a362f9b",
+    "url": "/static/js/main.a43d8407.chunk.js"
+  },
+  {
+    "revision": "e588f9a6b8f5492f8469",
+    "url": "/static/js/runtime-main.7838b856.js"
+  }
+]);
